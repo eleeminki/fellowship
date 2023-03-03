e84598011c5c5c1ce77f1c9a1cb0612c16f3275d
Eric <ericleeminki@gmail.com>
2023-03-03 14:29:45 -0800

    rewrite

--- core/router/routes.php
+++ core/router/routes.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    '/fellowship/' => 'controllers/home/index.php',
-    '/fellowship/login' => 'controllers/login/index.php',
-    '/fellowship/markets' => 'controllers/market/index.php',
-    '/fellowship/market' => 'controllers/market/single.php',
-    '/fellowship/market/post' => 'controllers/market/post.php'
+    '/' => 'controllers/home/index.php',
+    '/login' => 'controllers/login/index.php',
+    '/markets' => 'controllers/market/index.php',
+    '/market' => 'controllers/market/single.php',
+    '/market/post' => 'controllers/market/post.php'
 ];
 
 ?>
